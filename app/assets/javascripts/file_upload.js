028a15d362c21ca944956b2ff5efd786f1a3d37c
max <max@max-desktop.(none)>
2012-02-22 17:14:34 +0200

    small fix

--- app/assets/javascripts/file_upload.js
+++ app/assets/javascripts/file_upload.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         fileInput: $('#inputFile'),
         done: function (e, data) {
             $.each(data.result, function (index, file) {
-                $('#dropZone').append('<p>Attached file: ' + file.name + ' (' + file.size + ') <input type="hidden" name="files[]" value="' + file.id + '"></p>');
+                $('#dropZone').append('<p class="file_line">Attached file: ' + file.name + ' (' + file.size + ') <input type="hidden" name="files[]" value="' + file.id + '"></p>');
             });
         }
     });
