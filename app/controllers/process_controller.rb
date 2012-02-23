2533a280609ba7f118396aacede14b686a090aff
max <max@max-desktop.(none)>
2012-02-23 17:17:48 +0200

    Attachments view, assigning to process

--- app/controllers/process_controller.rb
+++ app/controllers/process_controller.rb
@@ -10,6 +10,17 @@ class ProcessController < ApplicationController
   def create
     @process = UserProcess.new(params[:user_process])
     if @process.save
+      
+      if params[:files_ids]
+        params[:files_ids].each do |id|
+          attachment = ProcessAttachment.find_by_id(id)
+          if attachment
+            attachment.process_id = @process.id
+            attachment.save
+          end
+        end
+      end
+      
       token_str = @process.get_owner_token_url
       redirect_to "/process/#{token_str}"
     end
@@ -18,6 +29,7 @@ class ProcessController < ApplicationController
   def show
     param = UserProcess.decrypt_token_url(params[:id])
     @process = UserProcess.find(param[:process_id])
+    p @process.process_attachments
     respond_with(@process)
   end
   
@@ -30,7 +42,7 @@ class ProcessController < ApplicationController
     attachment.attachment = params["files"][0]
     attachment.save
     viewHelper = Object.new.extend(ActionView::Helpers::NumberHelper)
-    render :json => ['name' => params["files"][0].original_filename, 'size' => viewHelper.number_to_human_size(File.size(params["files"][0].tempfile)), 'id' => rand(1000)]
+    render :json => ['name' => params["files"][0].original_filename, 'size' => viewHelper.number_to_human_size(File.size(params["files"][0].tempfile)), 'id' => attachment.id]
   end
   
   protected
